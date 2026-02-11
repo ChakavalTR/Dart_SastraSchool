ab10fca5b27f13f5c6cffe1b95eed24879464330
Thorng RattanakChakaval <ChakavalTR@yahoo.com>
2026-02-11 11:04:28 +0700

    more OOP

--- Chapter 5/Class_and_Objects.dart
+++ Chapter 5/Class_and_Objects.dart
@@ -11,6 +11,7 @@ class Car {
     print('Car: $model, Year: $year.');
   }
 }
+//!=============================================
 
 //!Inheritance
 class BatteryCar extends Car {
@@ -27,8 +28,72 @@ class BatteryCar extends Car {
     print('Battery Capacity: $battery kWh');
   }
 }
+//!=============================================
+
+//!Encapsulation (private)
+class Person {
+  String _name; //!Private Property , datatype must be follow below all
+
+  //!Constructor
+  Person(this._name);
+
+  //!Getter   (Getter to retrieve the name)
+  get getName => _name;
+
+  //!Setter   (Setter to set a new name) & set can replace void
+  set setName(String newName) => _name = newName;
+}
+//!=============================================
+
+//!Polymorphism
+class Animal {
+  void makeSound() {
+    print('Some generic sound');
+  }
+}
+
+//! @override is optional use&not use is fine
+class Dog extends Animal {
+  @override
+  void makeSound() {
+    print('Bark');
+  }
+}
+
+class Cat extends Animal {
+  void makeSound() {
+    print('Meow');
+  }
+}
+//!=============================================
+
+//! Abstract Class
+abstract class Shape {
+  void draw() {}
+}
+
+class Cirle extends Shape {
+  void draw() {
+    print('Drawing a cirle');
+  }
+}
+//!=============================================
+
+//! Interface class
+class Editable {
+  void eat() {
+    print('Eating food');
+  }
+}
+
+class Apple implements Editable {
+  void eat() {
+    print('Eating an apple');
+  }
+}
+//!=============================================
 
 void main() {
-  BatteryCar car = BatteryCar(model: 'Tesla Model Y', year: 2026, battery: 100);
-  car.Display();
+  Editable edit = Apple();
+  edit.eat();
 }
